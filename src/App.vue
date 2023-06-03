5af27d0ebb5e80826d44eca5179ecb2cae8e314d
Kevin <kevin@capitainecode.fr>
2023-06-03 14:36:39 +0200

    create a fonction that displays components on responsive

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <headerComponent />
-  <div>
-    <img-carousel />
+  <div v-if="!mobile">
+    <headerComponent/>
   </div>
-  <card-loop>
-    <cardActu />
-  </card-loop>
 
-  <title-banner />
-  <cards-section />
-  <footer-component />
+  <div v-if="mobile">
+    <MobileHeader />
+  </div>
+
+
+  <div>
+    <img-carousel/>
+  </div>
+
+  <div v-if="!mobile">
+    <card-loop>
+      <cardActu/>
+    </card-loop>
+  </div>
+
+  <div v-if="mobile">
+    <cards-section/>
+
+  </div>
+
+
+  <title-banner/>
+
+  <div v-if="mobile">
+    <card-loop>
+      <cardActu/>
+    </card-loop>
+  </div>
+
+  <div v-if="!mobile">
+    <cards-section/>
+
+  </div>
+
+
+  <footer-component/>
 </template>
 
 <script>
@@ -20,9 +49,11 @@ import TitleBanner from "@/components/titleBanner";
 import CardsSection from "@/components/cardsSection";
 import FooterComponent from "@/components/footerComponent";
 import CardLoop from "@/components/CardLoop";
+import MobileHeader from "@/components/MobileHeader";
 
 export default {
   name: 'App',
+  mobile: false,
   components: {
     CardLoop,
     FooterComponent,
@@ -30,20 +61,28 @@ export default {
     TitleBanner,
     ImgCarousel,
     headerComponent,
-    cardActu
+    cardActu,
+    MobileHeader
+  },
+
+  beforeCreate() {
+    this.mobile = document.documentElement.clientWidth <= 1024;
+
   }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-body{
-  margin:0;
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/MobileHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <button @click="toggleDropdown">Menu</button>
+    <div v-if="showDropdown">
+      <ul>
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "MobileHeader",
+
+  data() {
+    return {
+      showDropdown: false
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  display: inline-block;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  
+}
+
+li {
+  padding: 8px;
+  background-color: #f1f1f1;
+  border-bottom: 1px solid #ccc;
+}
+
+.dropdown {
+  display: none;
+}
+
+.show {
+  display: block;
+}
+.dropdown {
+  display: none;
+  transition: all 0.3s ease;
+}
+
+.show {
+  display: block;
+}
+</style>
